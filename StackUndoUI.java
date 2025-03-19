72a9bd2380eebc4a0994e12a2882a1a7f0e0ade6
Crestag7 <129108564+Crestag7@users.noreply.github.com>
2025-03-19 10:25:50 -0400

    Add files via upload

--- StackUndoUI.java
+++ StackUndoUI.java
@@ -1,21 +1,54 @@
 //Aadarsha Shrestha
 //This program creates a JavaFX UI that tracks user interactions and allows undoing actions using a stack.
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.util.Stack;
+
+class CustomStack<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    
+    private Node<T> top;
+    private int size = 0;
+    
+    public void push(T data) {
+        top = new Node<>(data, top);
+        size++;
+    }
+    
+    public T pop() {
+        if (top == null) return null;
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+    
+    public boolean isEmpty() {
+        return top == null;
+    }
+}
 
 public class StackUndoUI extends Application {
-    private Stack<String> actionStack = new Stack<>();
+    private CustomStack<State> undoStack = new CustomStack<>();
+    private CustomStack<State> redoStack = new CustomStack<>();
+    
     private RadioButton radio1, radio2, radio3;
     private CheckBox check1, check2, check3;
     private Slider slider;
     private ComboBox<String> dropdown;
-    private Button goButton, undoButton;
-
+    private Button goButton, undoButton, redoButton;
+    
     @Override
     public void start(Stage primaryStage) {
         radio1 = new RadioButton("Option 1");
@@ -37,60 +70,64 @@ public class StackUndoUI extends Application {
 
         goButton = new Button("Go");
         undoButton = new Button("Undo");
+        redoButton = new Button("Redo");
 
-        radio1.setOnAction(e -> recordAction("Radio", "1"));
-        radio2.setOnAction(e -> recordAction("Radio", "2"));
-        radio3.setOnAction(e -> recordAction("Radio", "3"));
-        check1.setOnAction(e -> recordAction("Check1", String.valueOf(check1.isSelected())));
-        check2.setOnAction(e -> recordAction("Check2", String.valueOf(check2.isSelected())));
-        check3.setOnAction(e -> recordAction("Check3", String.valueOf(check3.isSelected())));
-        slider.valueProperty().addListener((obs, oldVal, newVal) -> recordAction("Slider", String.valueOf(newVal.intValue())));
-        dropdown.setOnAction(e -> recordAction("Dropdown", dropdown.getValue()));
-
+        radio1.setOnAction(e -> recordState());
+        radio2.setOnAction(e -> recordState());
+        radio3.setOnAction(e -> recordState());
+        check1.setOnAction(e -> recordState());
+        check2.setOnAction(e -> recordState());
+        check3.setOnAction(e -> recordState());
+        slider.valueProperty().addListener((obs, oldVal, newVal) -> recordState());
+        dropdown.setOnAction(e -> recordState());
+        
         goButton.setOnAction(e -> showState());
         undoButton.setOnAction(e -> undoAction());
-
-        VBox layout = new VBox(10, radio1, radio2, radio3, check1, check2, check3, slider, dropdown, goButton, undoButton);
+        redoButton.setOnAction(e -> redoAction());
+        
+        VBox layout = new VBox(10, radio1, radio2, radio3, check1, check2, check3, slider, dropdown, goButton, undoButton, redoButton);
         Scene scene = new Scene(layout, 400, 400);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Simple Undo Example");
+        primaryStage.setTitle("Enhanced Undo Example");
         primaryStage.show();
     }
 
-    private void recordAction(String type, String value) {
-        actionStack.push(type + ":" + value);
+    private void recordState() {
+        undoStack.push(new State(radio1.isSelected(), radio2.isSelected(), radio3.isSelected(),
+                                 check1.isSelected(), check2.isSelected(), check3.isSelected(),
+                                 (int) slider.getValue(), dropdown.getValue()));
+        redoStack = new CustomStack<>(); // Clear redo stack when a new action occurs
     }
 
     private void undoAction() {
-        if (!actionStack.isEmpty()) {
-            String lastAction = actionStack.pop();
-            String[] parts = lastAction.split(":");
-            String type = parts[0];
-            String value = parts[1];
-
-            switch (type) {
-                case "Radio":
-                    radio1.setSelected(false);
-                    radio2.setSelected(false);
-                    radio3.setSelected(false);
-                    break;
-                case "Check1":
-                    check1.setSelected(!Boolean.parseBoolean(value));
-                    break;
-                case "Check2":
-                    check2.setSelected(!Boolean.parseBoolean(value));
-                    break;
-                case "Check3":
-                    check3.setSelected(!Boolean.parseBoolean(value));
-                    break;
-                case "Slider":
-                    slider.setValue(Double.parseDouble(value));
-                    break;
-                case "Dropdown":
-                    dropdown.setValue("Item 1");
-                    break;
-            }
+        if (!undoStack.isEmpty()) {
+            redoStack.push(getCurrentState());
+            restoreState(undoStack.pop());
         }
+    }
+    
+    private void redoAction() {
+        if (!redoStack.isEmpty()) {
+            undoStack.push(getCurrentState());
+            restoreState(redoStack.pop());
+        }
+    }
+
+    private State getCurrentState() {
+        return new State(radio1.isSelected(), radio2.isSelected(), radio3.isSelected(),
+                         check1.isSelected(), check2.isSelected(), check3.isSelected(),
+                         (int) slider.getValue(), dropdown.getValue());
+    }
+
+    private void restoreState(State state) {
+        radio1.setSelected(state.radio1);
+        radio2.setSelected(state.radio2);
+        radio3.setSelected(state.radio3);
+        check1.setSelected(state.check1);
+        check2.setSelected(state.check2);
+        check3.setSelected(state.check3);
+        slider.setValue(state.sliderValue);
+        dropdown.setValue(state.dropdownValue);
     }
 
     private void showState() {
@@ -103,6 +140,24 @@ public class StackUndoUI extends Application {
         Alert alert = new Alert(Alert.AlertType.INFORMATION, state);
         alert.setTitle("Current State");
         alert.showAndWait();
+    }
+
+    private static class State {
+        boolean radio1, radio2, radio3;
+        boolean check1, check2, check3;
+        int sliderValue;
+        String dropdownValue;
+
+        State(boolean r1, boolean r2, boolean r3, boolean c1, boolean c2, boolean c3, int slider, String dropdown) {
+            this.radio1 = r1;
+            this.radio2 = r2;
+            this.radio3 = r3;
+            this.check1 = c1;
+            this.check2 = c2;
+            this.check3 = c3;
+            this.sliderValue = slider;
+            this.dropdownValue = dropdown;
+        }
     }
 
     public static void main(String[] args) {
